0fa08994db29ea3404a977cf07d707194c91495d
Lohit Bura <lohitbura@gmail.com>
2021-12-02 22:43:21 +0530

    Upgraded All Packages

--- lib/main.dart
+++ lib/main.dart
@@ -43,6 +43,7 @@ class MyHttpOverrides extends HttpOverrides {
   @override
   HttpClient createHttpClient(SecurityContext? context) {
     return super.createHttpClient(context)
+
       ..badCertificateCallback =
           (X509Certificate cert, String host, int port) => true;
   }
