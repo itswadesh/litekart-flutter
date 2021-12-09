1574efddf1621f8b04c9623e31d5e787ba3eab5f
Lohit Bura <lohitbura@gmail.com>
2021-12-09 11:53:27 +0530

    Update SceneDelegate.swift

--- ios/Runner/SceneDelegate.swift
+++ ios/Runner/SceneDelegate.swift
@@ -1,1 +1,15 @@
 
+import FacebookCore
+ 
+func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+    guard let url = URLContexts.first?.url else {
+        return
+    }
+
+    ApplicationDelegate.shared.application(
+        UIApplication.shared,
+        open: url,
+        sourceApplication: nil,
+        annotation: [UIApplication.OpenURLOptionsKey.annotation]
+    )
+}
